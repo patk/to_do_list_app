0f626d0e1bbf0007e982a7afc94163bb1ccf6da0
Pat <pat.ksarach@gmail.com>
2021-03-22 10:36:46 +1100

    Add new attributes and tables to the database

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,8 +1,20 @@
+DROP TABLE IF EXISTS lists;
+
+CREATE TABLE IF NOT EXISTS lists (
+    list_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    list_name VARCHAR(100) NOT NULL
+);
+
 DROP TABLE IF EXISTS tasks;
 
 CREATE TABLE IF NOT EXISTS tasks (
     task_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     task_description VARCHAR(100) NOT NULL,
-    create_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    is_complete BOOLEAN NOT NULL
+    list_id INT NOT NULL,
+    date_time TIMESTAMPTZ NOT NULL DEFAULT now(),
+    is_complete BOOLEAN NOT NULL,
+
+    FOREIGN KEY(list_id)
+    REFERENCES lists(list_id)
+    ON DELETE CASCADE
 );
--- sql/seed_table.sql
+++ sql/seed_table.sql
@@ -1,7 +1,18 @@
-INSERT INTO tasks (task_description, create_at, is_complete)
+INSERT INTO lists (list_name)
+VALUES
+('Personal'),
+('Work'),
+('Study');
+
+INSERT INTO tasks (task_description, list_id, date_time, is_complete)
 VALUES 
-('Buy groceries', '2021-03-01 09:30:00+11', false),
-('Pay electricity bill', '2021-03-01 09:30:00+11', false),
-('Develop to-do list app', '2021-03-01 09:30:00+11', false),
-('Pick up furniture', '2021-03-01 09:30:00+11', false),
-('Set up new laptop', '2021-03-01 09:30:00+11', false);
+('Buy groceries', 1, '2021-03-01 09:30:00+11', false),
+('Pay electricity bill', 1, '2021-03-01 09:30:00+11', false),
+('Pick up furniture', 1, '2021-03-01 09:30:00+11', false),
+('Develop to-do list app', 2, '2021-03-01 09:30:00+11', false),
+('Organise meeting with client', 2, '2021-03-01 09:30:00+11', false),
+('Fix app bugs', 2, '2021-03-01 09:30:00+11', false),
+('Do daily Duolingo lesson', 3, '2021-03-01 09:30:00+11', false),
+('Learn jQuery', 3, '2021-03-01 09:30:00+11', false),
+('Research API', 3, '2021-03-01 09:30:00+11', false);
+
